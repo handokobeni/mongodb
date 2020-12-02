886eb5e8377ac9c1e108ca134f609c476c02f3cb
handoko78 <handoko.ben@gagasimaji.com>
2020-12-02 12:55:31 +0700

    query

---
+++ learn/3-query.js
@@ -0,0 +1,25 @@
+// select *  from customers where _id = 'beni'
+db.customers.find({
+    _id: "beni"
+});
+
+// select *  from customers where _id = 'beni' and name = 'Beni Handoko
+db.customers.find({
+    _id: "beni",
+    name: "Beni Handoko"
+});
+
+// select * from customers where name = 'Beni Handoko'
+db.customers.find({
+    name: "Beni Handoko"
+});
+
+// select * from products where price = 2000
+db.products.find({
+    price: 2000
+});
+
+// select * from orders where items.product_id = 1
+db.orders.find({
+    "items.product_id": 1
+});
